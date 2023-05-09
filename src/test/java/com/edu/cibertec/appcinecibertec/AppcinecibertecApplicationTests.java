82818d4803309464ea0db78673db8f235b10340a
y4zper <ander.crr@gmail.com>
2023-05-08 19:38:46 -0500

    initial commit

---
+++ src/test/java/com/edu/cibertec/appcinecibertec/AppcinecibertecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.cibertec.appcinecibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcinecibertecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
